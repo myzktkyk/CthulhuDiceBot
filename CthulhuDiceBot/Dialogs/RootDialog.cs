6514ff2117eb2d39700a5846b2361bf40e04c99e
miyazaki <miyazaki@nextscape.net>
2017-04-21 19:37:20 +0900

    現状最新をコミット。

--- CthulhuDiceBot/Dialogs/RootDialog.cs
+++ CthulhuDiceBot/Dialogs/RootDialog.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.Bot.Builder.Dialogs;
+﻿using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace CthulhuDiceBot.Dialogs
 {
     [Serializable]
     public class RootDialog : IDialog<object>
     {
+
         public Task StartAsync(IDialogContext context)
         {
             context.Wait(MessageReceivedAsync);
@@ -19,13 +21,41 @@ namespace CthulhuDiceBot.Dialogs
         {
             var activity = await result as Activity;
 
-            // calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
-
             // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+            await context.PostAsync(CommandFactory(activity)?.Invoke(activity));
 
             context.Wait(MessageReceivedAsync);
+        }
+        private Func<Activity, string> CommandFactory(Activity activity)
+        {
+            if (Regex.IsMatch(activity.Text, @"[d|D]ice\s+(\d+)[d|D](\d+)$"))
+            {
+                return (a) =>
+                {
+                    var regex = new Regex(@"[d|D]ice\s+(\d+)[d|D](\d+)$");
+                    var group = regex.Match(activity.Text).Groups;
+
+                    var left = int.Parse(group[1].Value);
+                    var right = int.Parse(group[2].Value);
+
+                    int result = 0;
+                    for (int i = 0; i < left; i++)
+                    {
+                        var generator = new Random();
+                        result += generator.Next(1, right);
+                    }
+
+                    return result.ToString();
+                };
+            }
+            else if (Regex.IsMatch(activity.Text, "[U|u]sage"))
+            {
+                return (a) => { return "avalable commands\nusage\ndice (n)D(m)"; };
+            }
+            else
+            {
+                return (a) => { return $"Sorry I don't know command [{a.Text}]."; };
+            }
         }
     }
 }
---
+++ CthulhuDiceBot/Factories/CommandFactory.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace CthulhuDiceBot.Factories
+{
+
+    public class CommandFactory
+    {
+        public readonly Dictionary<string, Tuple<Regex, Func<Activity, string>>> dictionary
+            = new Dictionary<string, Tuple<Regex, Func<Activity, string>>>();
+        
+        private CommandFactory()
+        {
+            {
+                var regex = new Regex(@"[d|D]ice\s+(\d+)[d|D](\d+)$");
+                dictionary.Add(
+                    "Dice",
+                    Tuple.Create<Regex, Func<Activity, string>>(
+                        regex,
+                        (a) =>
+                            {
+                                var group = regex.Match(a.Text).Groups;
+                                var left = int.Parse(group[1].Value);
+                                var right = int.Parse(group[2].Value);
+
+                                int result = 0;
+                                for (int i = 0; i < left; i++)
+                                {
+                                    var generator = new Random();
+                                    result += generator.Next(1, right);
+                                }
+                                return result.ToString();
+                            }
+                    )
+                );
+            }
+            {
+                var regex = new Regex("[U|u]sage");
+                dictionary.Add(
+                    "Usage",
+                    Tuple.Create<Regex, Func<Activity, string>>(
+                        regex,
+                        (a) => { return "avalable commands\nusage\ndice (n)D(m)"; }
+                    )
+                );
+            }
+        }
+    }
+}
