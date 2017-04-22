08a39dd1e7d64ea7b58da1d7b19f884535ff2cfb
miyazaki <miyazaki@nextscape.net>
2017-04-22 16:27:25 +0900

    Regist実装

--- CthulhuDiceBot/Dialogs/RootDialog.cs
+++ CthulhuDiceBot/Dialogs/RootDialog.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Bot.Builder.Dialogs;
+﻿using CthulhuDiceBot.Factories;
+using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
 using System;
 using System.Text.RegularExpressions;
@@ -22,40 +23,9 @@ namespace CthulhuDiceBot.Dialogs
             var activity = await result as Activity;
 
             // return our reply to the user
-            await context.PostAsync(CommandFactory(activity)?.Invoke(activity));
+            await context.PostAsync(CommandFactory.Instance.GetCommand(activity).Invoke(activity));
 
             context.Wait(MessageReceivedAsync);
-        }
-        private Func<Activity, string> CommandFactory(Activity activity)
-        {
-            if (Regex.IsMatch(activity.Text, @"[d|D]ice\s+(\d+)[d|D](\d+)$"))
-            {
-                return (a) =>
-                {
-                    var regex = new Regex(@"[d|D]ice\s+(\d+)[d|D](\d+)$");
-                    var group = regex.Match(activity.Text).Groups;
-
-                    var left = int.Parse(group[1].Value);
-                    var right = int.Parse(group[2].Value);
-
-                    int result = 0;
-                    for (int i = 0; i < left; i++)
-                    {
-                        var generator = new Random();
-                        result += generator.Next(1, right);
-                    }
-
-                    return result.ToString();
-                };
-            }
-            else if (Regex.IsMatch(activity.Text, "[U|u]sage"))
-            {
-                return (a) => { return "avalable commands\nusage\ndice (n)D(m)"; };
-            }
-            else
-            {
-                return (a) => { return $"Sorry I don't know command [{a.Text}]."; };
-            }
         }
     }
 }
--- CthulhuDiceBot/Factories/CommandFactory.cs
+++ CthulhuDiceBot/Factories/CommandFactory.cs
@@ -8,44 +8,126 @@ namespace CthulhuDiceBot.Factories
 
     public class CommandFactory
     {
-        public readonly Dictionary<string, Tuple<Regex, Func<Activity, string>>> dictionary
+
+        #region static readonly fields
+
+        private static readonly Dictionary<string, Tuple<Regex, Func<Activity, string>>> dictionary
             = new Dictionary<string, Tuple<Regex, Func<Activity, string>>>();
-        
+
+        private static readonly Regex DiceCommandRegex = new Regex(@"dice\s+(\d+)[d|D](\d+)$");
+
+        private static readonly Regex UsageCommandRegex = new Regex("usage$");
+
+        private static readonly Regex MentionRegex = new Regex(@"^@\w+\s+");
+
+        private static readonly Regex RegistRegex = new Regex(@"regist\s+(\d+)/(\d+)$");
+
+        #endregion
+
+        #region properties
+
+        public static CommandFactory Instance { get; private set; } = new CommandFactory();
+
+        #endregion
+
+        #region constractor
+
         private CommandFactory()
         {
-            {
-                var regex = new Regex(@"[d|D]ice\s+(\d+)[d|D](\d+)$");
-                dictionary.Add(
-                    "Dice",
-                    Tuple.Create<Regex, Func<Activity, string>>(
-                        regex,
-                        (a) =>
-                            {
-                                var group = regex.Match(a.Text).Groups;
-                                var left = int.Parse(group[1].Value);
-                                var right = int.Parse(group[2].Value);
-
-                                int result = 0;
-                                for (int i = 0; i < left; i++)
-                                {
-                                    var generator = new Random();
-                                    result += generator.Next(1, right);
-                                }
-                                return result.ToString();
-                            }
-                    )
-                );
-            }
-            {
-                var regex = new Regex("[U|u]sage");
-                dictionary.Add(
-                    "Usage",
-                    Tuple.Create<Regex, Func<Activity, string>>(
-                        regex,
-                        (a) => { return "avalable commands\nusage\ndice (n)D(m)"; }
-                    )
-                );
-            }
+            dictionary.Add(
+                "Dice",
+                Tuple.Create<Regex, Func<Activity, string>>(DiceCommandRegex, (a) => CalculateDice(a))
+            );
+            dictionary.Add(
+                "Usage",
+                Tuple.Create<Regex, Func<Activity, string>>(UsageCommandRegex, (a) => { return "avalable commands\nusage\ndice (n)D(m)"; })
+            );
+            dictionary.Add(
+                "Regist",
+                Tuple.Create<Regex, Func<Activity, string>>(RegistRegex, (a) => CalculateRegist(a))
+            );
         }
+
+        #endregion
+
+        #region public method(s)
+
+        public Func<Activity, string> GetCommand(Activity activity)
+        {
+            Func<Activity, string> command = null;
+            foreach (var entry in dictionary.Values)
+            {
+                if (entry.Item1.IsMatch(activity.Text.ToLower()))
+                {
+                    command = entry.Item2;
+                    break;
+                }
+            }
+            if (command == null)
+            {
+                command = (a) => { return $"Sorry I don't know command [{a.Text}]."; };
+            }
+            return command;
+        }
+
+        public bool IsCommand(Activity activity)
+        {
+            bool result = false;
+            if (MentionRegex.IsMatch(activity.Text))
+            {
+                foreach (var entry in dictionary.Values)
+                {
+                    if (entry.Item1.IsMatch(activity.Text.ToLower()))
+                    {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        #endregion
+
+        #region private method(s)
+
+        private string CalculateDice(Activity activity)
+        {
+            var group = DiceCommandRegex.Match(activity.Text).Groups;
+            var left = int.Parse(group[1].Value);
+            var right = int.Parse(group[2].Value);
+
+            int result = 0;
+            for (int i = 0; i < left; i++)
+            {
+                var generator = new Random();
+                result += generator.Next(1, right);
+            }
+            return result.ToString();
+        }
+
+        private string CalculateRegist(Activity activity)
+        {
+            var group = RegistRegex.Match(activity.Text).Groups;
+            var active = int.Parse(group[1].Value);
+            var passive = int.Parse(group[2].Value);
+
+            int target = (active - passive) * 5 + 50;
+            int dice = new Random().Next(1, 100);
+            bool isSuccess = dice <= target;
+
+            string result = null;
+            if (isSuccess)
+            {
+                result = $"Regist succeeded. Target = {target} Dice = {dice}";
+            }
+            else
+            {
+                result = $"Regist failed. Target = {target} Dice = {dice}";
+            }
+            return result;
+        }
+
+        #endregion
     }
 }
