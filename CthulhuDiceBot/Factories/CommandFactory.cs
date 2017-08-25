1b67a4128db43bb2d5911594027fd88316fb3295
miyazaki <miyazaki@nextscape.net>
2017-08-25 16:40:10 +0900

    リファクタリング+LUISで新しく定義したEntity(Skill)に対応させる。

--- CthulhuDiceBot/Factories/CommandFactory.cs
+++ CthulhuDiceBot/Factories/CommandFactory.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.Bot.Connector;
 using Microsoft.Cognitive.LUIS;
 using System;
-using System.Linq;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace CthulhuDiceBot.Factories
@@ -14,7 +14,7 @@ namespace CthulhuDiceBot.Factories
 
         #region static readonly fields
 
-        private static readonly Dictionary<string, Func<Activity, LuisResult, string>> dictionary = new Dictionary<string, Func<Activity, LuisResult, string>>();
+        private static readonly Dictionary<string, Func<Activity, LuisResult, string>> Dictionary = new Dictionary<string, Func<Activity, LuisResult, string>>();
 
         private static readonly Regex MentionRegex = new Regex(@"^@\w+\s+");
 
@@ -34,9 +34,9 @@ namespace CthulhuDiceBot.Factories
 
         private CommandFactory()
         {
-            dictionary.Add("DiceRoll", (a, r) => CalculateDice(a, r));
-            dictionary.Add("Regist", (a, r) => CalculateRegist(a, r));
-            dictionary.Add("Judge", (a, r) => CalculateJudge(a, r));
+            Dictionary.Add("DiceRoll", CalculateDice);
+            Dictionary.Add("Regist", CalculateRegist);
+            Dictionary.Add("Judge", CalculateJudge);
         }
 
         #endregion
@@ -45,31 +45,24 @@ namespace CthulhuDiceBot.Factories
 
         public Func<Activity, LuisResult, string> GetCommand(Activity activity)
         {
-            LuisResult response = LuisClient.Predict(activity.Text).Result;
-            Func<Activity, LuisResult, string> command = dictionary[response.TopScoringIntent.Name];
-            if (command == null)
-            {
-                command = (a, r) => { return $"Sorry I don't know command [{a.Text}]."; };
-            }
+            var response = LuisClient.Predict(activity.Text).Result;
+            var command = Dictionary[response.TopScoringIntent.Name] ?? ((a, r) => $"Sorry I don't know command [{a.Text}].");
             return command;
         }
 
         public string ExecuteCommand(Activity activity)
         {
-            LuisResult response = LuisClient.Predict(activity.Text).Result;
-            Func<Activity, LuisResult, string> command = dictionary[response.TopScoringIntent.Name];
+            var response = LuisClient.Predict(activity.Text).Result;
+            var command = Dictionary[response.TopScoringIntent.Name];
             return command.Invoke(activity, response);
         }
 
         public bool IsCommand(Activity activity)
         {
-            bool result = false;
-            bool isGroup = activity.Conversation.IsGroup ?? false;
-            if (!isGroup || MentionRegex.IsMatch(activity.Text.ToLower()))
-            {
-                LuisResult response = LuisClient.Predict(activity.Text).Result;
-                result = dictionary.ContainsKey(response.TopScoringIntent.Name);
-            }
+            var isGroup = activity.Conversation.IsGroup ?? false;
+            if (isGroup && !MentionRegex.IsMatch(activity.Text.ToLower())) return false;
+            var response = LuisClient.Predict(activity.Text).Result;
+            var result = Dictionary.ContainsKey(response.TopScoringIntent.Name);
             return result;
         }
 
@@ -87,13 +80,13 @@ namespace CthulhuDiceBot.Factories
             {
                 throw new ArgumentException("１以上の数字を指定してください！");
             }
-            int dice = 0;
-            for (int i = 0; i < left; i++)
+            var dice = 0;
+            for (var i = 0; i < left; i++)
             {
                 var generator = new Random();
                 dice += generator.Next(1, right);
             }
-            return $"出目は{dice.ToString()}だよ！";
+            return $"出目は{dice}だよ！";
         }
 
         private string CalculateRegist(Activity activity, LuisResult luisResult)
@@ -101,56 +94,35 @@ namespace CthulhuDiceBot.Factories
             var active = int.Parse(luisResult.Entities["Active"].FirstOrDefault().Value);
             var passive = int.Parse(luisResult.Entities["Passive"].FirstOrDefault().Value);
 
-            int target = (active - passive) * 5 + 50;
-            int dice = new Random().Next(1, 100);
-            bool isSuccess = dice <= target;
+            var target = (active - passive) * 5 + 50;
+            var dice = new Random().Next(1, 100);
+            var isSuccess = dice <= target;
 
-            string result = null;
-            if (isSuccess)
-            {
-                result = $"抵抗成功！ 目標値 = {target} ダイス = {dice}";
-            }
-            else
-            {
-                result = $"抵抗失敗・・・。 目標値 = {target} ダイス = {dice}";
-            }
+            var result = isSuccess ? $"抵抗成功！ 目標値 = {target} ダイス = {dice}" : $"抵抗失敗・・・。 目標値 = {target} ダイス = {dice}";
             return result;
         }
 
         private string CalculateJudge(Activity activity, LuisResult luisResult)
         {
-            var target = int.Parse(luisResult.Entities["TargetValue"].FirstOrDefault().Value);
+            var target = int.Parse(luisResult.Entities["TargetValue"].FirstOrDefault()?.Value);
+            var skill = luisResult.Entities["Skill"].FirstOrDefault()?.Value;
 
-            int dice = 0;
-            for (int i = 0; i < 1; i++)
+            var dice = 0;
+            for (var i = 0; i < 1; i++)
             {
                 var generator = new Random();
                 dice += generator.Next(1, 100);
             }
 
-            string result = null;
-            bool isSuccess = (dice <= target);
+            string result;
+            var isSuccess = dice <= target;
             if (isSuccess)
             {
-                if (dice == target)
-                {
-                    result = $"いちたりた！ {dice}/{target}";
-                }
-                else
-                {
-                    result = $"成功！ {dice}/{target}";
-                }
+                result = dice == target ? $"{skill}いちたりた！ {dice}/{target}" : $"{skill}成功！ {dice}/{target}";
             }
             else
             {
-                if (dice - target == 1)
-                {
-                    result = $"いちたりない・・・。 {dice}/{target}";
-                }
-                else
-                {
-                    result = $"失敗・・・。 {dice}/{target}";
-                }
+                result = dice - target == 1 ? $"{skill}いちたりない・・・。 {dice}/{target}" : $"{skill}失敗・・・。 {dice}/{target}";
             }
             return result;
         }
